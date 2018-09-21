67862e1b8b7366198ff16ca43ab0cb3e01364c5e
baseem <baseem@gmail.com>
2018-09-20 15:45:18 -1000

    project update

--- index.js
+++ index.js
@@ -1,39 +1,34 @@
 const express = require('express');
 const app = express();
-const axios = require('axios');
+const baseem = require('baseem');
+const DarkSky = require('dark-sky')
 require('dotenv').config()
+const darksky = new DarkSky(process.env.DARKSKY_API_KEY);
+const beaufort = require('beaufort')
+const options = {unit: 'kmh', getName: true};
+const axios = require('axios');
 
 
-// NOAA API - Datasets
-// https://www.ncdc.noaa.gov/cdo-web/webservices/v2
+const degToCompass = (val_num) => {
+    let num = parseInt(val_num);
+    let val = Math.floor((num / 22.5) + 0.5);
+    let arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+    return arr[(val % 16)];
+}
 
-
-
-
-let token = process.env.NOAA_API_KEY;
-
-
-
-app.get('/', (req,res) => {
-	let startdate = '2010-05-01';
-	let enddate = '2010-05-01';
-	let url = `https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&locationid=ZIP:28801&startdate=${startdate}&enddate=${enddate}
-`;
-	axios.get(url, {headers: { 
-			token: token
-		}
-	})
+// sample api request -> http://localhost:9000/2017/3/21.308714/-157.808782/8
+app.get('/', (req, res) => {
+	let lat = 21.282;
+	let lng = -157.661
+	axios.get(`https://api.darksky.net/forecast/${darksky.apiKey}/${lat},${lng}`)
 	.then(response => {
-		res.json(response.data)
+
+		res.json(response.data);
+	}).catch(err => {
+		res.json('its probably raining...')
 	})
-	.catch(err => {
-		console.log(err);
-		res.json('oh nooooooooo! it brokesh*t')
-	})
-});
-
-
-
+})
 
 app.listen(9000);
+
 
